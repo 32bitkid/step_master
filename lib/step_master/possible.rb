8aeda2135425576e988ca9c4abfee6b8e21b718e
Jim Holmes <32bitkid@gmail.com>
2009-10-20 23:55:17 +0800

    bump to 0.0.4 started architecture for multiple formats terminals return actual terminal elements for easy file_path and line_number attributes

--- lib/step_master/possible.rb
+++ lib/step_master/possible.rb
@@ -2,20 +2,27 @@ module StepMaster
 	class Possible < ::Hash
 		EMPTY = Possible.new		
 		
-		def terminal!(result)
-			@terminal = result.freeze
+		attr_reader :file_path, :line_number
+		
+		def terminal!(result, options = {})
+			@terminal = true
+			@result = result
+			@file_path = options[:file_path].freeze
+			@line_number = options[:line_number].freeze
 		end
 		
 		def terminal?
-			!@terminal.nil?
-		end
-		
-		def result
 			@terminal
 		end
 		
-		def inspect
-			super << ((terminal? )? "["+result+"]" : "")
+		def result
+			@result
+		end
+		
+		def file
+			if @file_path
+				[@file_path, @line_number].compact.join(":")
+			end
 		end
 	end
 end
